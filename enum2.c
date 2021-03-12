5639fa9304a864cde317b30c44f0774937c062e6
Tugba <77529943+Tugbaaa38@users.noreply.github.com>
2021-03-12 10:16:09 +0300

    Add files via upload

---
+++ enum2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//enum kullanimi: Bu kullanimda kendi veri tipimizi olusturabiliriz.Ornegin aylar listesi olusturmak isteyelim ve bu aylari belli mevsimlere bolerek sinirlandiralim,ya da boolean diye bir 
+//degisken olusturup true degerine 1 false degerine 0 atayalim cunku baska ihtimal yoktur ya 1 ya da 0 olacaktir.Bu tarz islemler icin enum yapisini kullaniriz.
+//enum da biz deger vermesekte elemanlar 0,1,2,.. seklinde numaralandirilir.
+int main() {
+	
+	enum Mevsimler{
+		Ilkbahar=1,
+		Yaz,
+		Sonbahar,
+		Kis
+	}hangiMevsim;
+	
+	hangiMevsim=4;
+	
+	switch(hangiMevsim)
+	{
+		case 1:
+			printf("Mart-Nisan-Mayis");
+			break;
+		case 2:
+			printf("Haziran-Temmuz-Agustos");
+			break;
+		case 3:
+			printf("Eylul-Ekim-Kasim");
+			break;
+		case 4:
+		    printf("Aralik-Ocak-Subat");
+		    break;
+		default:
+			printf("Boyle bir mevsim yok :)");
+		    break;
+	}
+	return 0;
+}
